b60a9523da227275ad0b9a6ea32ff610033e6400
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-01-18 17:32:26 +0530

    registration module js and html update

---
+++ www/js/registration.js
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener("offline", checkConnection, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        // app.receivedEvent('deviceready');
+		$('#btnReg').click(function () { 
+									 
+        
+		
+            var first_name = $('#first_name').val();
+            var last_name = $('#last_name').val();
+            var email = $("#email").val();
+            var mobile = $("#mobilemobile").val();
+            var password = $("#passwordpassword").val();
+            var confirm_password = $("#confirm_passwordconfirm_password").val();
+
+            var datas = { 'first_name': first_name, 'last_name': last_name, 'email': email, 'mobile': mobile, 'password': password, 'confirm_password': confirm_password };
+            if (first_name == "" || last_name == "" || email == "" || mobile == "" || password == "" || confirm_password=="")
+            {
+                if (first_name == ""){
+                    $('#first_name').css('border-color', 'red');
+                }
+                if (last_name == "") {
+                    $('#last_name').css('border-color', 'red');
+                }
+                if (email == "") {
+                    $('#email').css('border-color', 'red');
+                }
+                if (mobile == "") {
+                    $('#mobile').css('border-color', 'red');
+                }
+                if (password == "") {
+                    $('#password').css('border-color', 'red');
+                }
+                if (confirm_password == "") {
+                    $('#confirm_password').css('border-color', 'red');
+                }
+            }
+            else{
+				$(".se-pre-con").show();
+                $.ajax({
+                    type: "post",
+                    url: "https://bebongstore.com/nxias/manage_api/registration",
+                    data: datas,
+					datatype:'json',
+                    beforeSend: function () {
+                        $('#btnReg').prop('disabled', true);
+                    },
+                    success: function (response) {
+                        var da = $.parseJSON(response);
+                        // console.log(d.status);
+                        $('#btnReg').prop('disabled', false);
+                        if (da.status==1){
+							$('#msg').css('display','none');
+                             window.location.href = "home.html";   
+                        }
+                        else if (da.status == 2){
+							navigator.notification.beep(1);
+							showAlert();
+							window.location.href = "login.html";
+                        }
+						else {
+                            // console.log('Mismatch');
+							navigator.notification.beep(1);
+							showcnfpass();
+							window.location.href = "registration.html"; 
+						}
+                    }
+                });
+            }
+		});	
+    },
+    
+};
+function showAlert() {
+    navigator.notification.alert(
+        'Email Already Exist.Please Login.',  // message
+        alertDismissed,         // callback
+        'Already Registered !',            // title
+        'OK'                  // buttonName
+    );
+}
+function alertDismissed() {
+    // do something
+}
+function showcnfpass() {
+    navigator.notification.alert(
+        'Password and Confirm Password Do Not Match.',  // message
+        alertDismissed,         // callback
+        'Mismatch!',            // title
+        'OK'                  // buttonName
+    );
+}
+// This Function For Check Internet Connection
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+
+    // alert('Connection type: ' + states[networkState]);
+    alert('No Internet Connection');
+    // navigator.app.exitApp();
+}
