567fde5374141990983d8d3f2db22d2e1bdfb3bc
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-01-23 18:30:28 +0530

    Subject Page Impliment Success

---
+++ www/js/subject.js
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function () {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        // document.addEventListener("backbutton", onBackKeyDown, false);
+        document.addEventListener("offline", checkConnection, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function () {
+
+        // Get All Subject
+        $.ajax({
+            type: "post",
+            url: "https://bebongstore.com/vhelp/manage_api/get_subject",
+            data: "data",
+            dataType: "json",
+            beforeSend: function () {
+              $(".se-pre-con").show();
+            },
+            success: function (response) {
+                var count=1;
+                $.each(response.subjects, function (val, text) { 
+                    var subjectImage = text.subject_image;
+                    var subjectName = text.subject_name;
+                    var subjectDescription = text.description;
+
+                    if(count==1){
+                        var className="in";
+                    }
+                    else{
+                        var className = "";
+                    }
+                    
+                    $('#accordion').append('<div class="panel panel-default"><div class= "panel-heading"><h4 class="panel-title" style="font-size:40px;"><a data-toggle="collapse" data-parent="#accordion" href="#collapse' + count + '" style="text-decoration:none;"><img src="https://bebongstore.com/vhelp/uploads/subject/' + subjectImage + '" width="128" /> ' + subjectName + '</a></h4></div ><div id="collapse' + count + '" class="panel-collapse collapse '+className+'"><div class="panel-body" style="font-size:40px;">' + subjectDescription +'</div></div></div >');
+                    count++;
+                });
+            }
+        });
+
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+        console.log('Received Event: ' + id);
+    }
+
+};
+
+// This Function For Check Internet Connection
+function checkConnection() {
+    var networkState = navigator.connection.type;
+
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+
+    // alert('Connection type: ' + states[networkState]);
+    navigator.notification.alert(
+        'No Internet Connection.',
+        alertDismissed,
+        'Alert!',
+        'OK'
+    );
+    // navigator.app.exitApp();
+}
+
+
