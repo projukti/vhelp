04273df3bde824112055f53d2c8e42e71adcfaf5
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-01-29 15:45:47 +0530

    Push notification plugin remove

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -509,54 +509,6 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.screenorientation"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-badge/www/badge.js",
-        "id": "cordova-plugin-badge.Badge",
-        "pluginId": "cordova-plugin-badge",
-        "clobbers": [
-            "cordova.plugins.notification.badge"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-badge/src/browser/favico.min.js",
-        "id": "cordova-plugin-badge.Badge.Favico",
-        "pluginId": "cordova-plugin-badge",
-        "clobbers": [
-            "cordova.plugins.notification.badge.Favico"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-badge/src/browser/BadgeProxy.js",
-        "id": "cordova-plugin-badge.Badge.Proxy",
-        "pluginId": "cordova-plugin-badge",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-local-notification/www/local-notification.js",
-        "id": "cordova-plugin-local-notification.LocalNotification",
-        "pluginId": "cordova-plugin-local-notification",
-        "clobbers": [
-            "cordova.plugins.notification.local"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-local-notification/www/local-notification-core.js",
-        "id": "cordova-plugin-local-notification.LocalNotification.Core",
-        "pluginId": "cordova-plugin-local-notification",
-        "clobbers": [
-            "cordova.plugins.notification.local.core",
-            "plugin.notification.local.core"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-local-notification/www/local-notification-util.js",
-        "id": "cordova-plugin-local-notification.LocalNotification.Util",
-        "pluginId": "cordova-plugin-local-notification",
-        "merges": [
-            "cordova.plugins.notification.local.core",
-            "plugin.notification.local.core"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -578,9 +530,7 @@ module.exports.metadata =
     "cordova-plugin-file": "6.0.1",
     "cordova-plugin-media-capture": "3.0.1",
     "cordova-plugin-x-toast": "2.6.0",
-    "cordova-plugin-screen-orientation": "3.0.1",
-    "cordova-plugin-badge": "0.8.7",
-    "cordova-plugin-local-notification": "0.9.0-beta.2"
+    "cordova-plugin-screen-orientation": "3.0.1"
 }
 // BOTTOM OF METADATA
 });
